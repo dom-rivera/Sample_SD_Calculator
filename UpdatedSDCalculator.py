a33d25de68ddf827ef0de8d0ef58d96307b5f6fc
Dom Rivera <94301308+dom-rivera@users.noreply.github.com>
2023-09-13 15:53:59 +0800

    Update UpdatedSDCalculator.py
    Power function added

--- UpdatedSDCalculator.py
+++ UpdatedSDCalculator.py
@@ -1,15 +1,18 @@
-#Sample Standard deviation calculator
+# Sample Standard Deviation Formula
+
 Greatings = input(
   "Hello! Welcome to Compute Sample SD! \n\nType Start to begin. ")
 sum_of_x_minus_mean = input(
-  "What is the value of your sum of X minus mean squared (X-Xbar)² ?\n ")
+  "What is the value of your sum of X minus mean squared (X-Xbar) ?\n ")
 number_of_samples = input("How many samples do you have in your study?\n")
 #Float Conversion
 float_sum_of_x_minus_mean = float(sum_of_x_minus_mean)
 float_number_of_samples = float(number_of_samples)
 Degrees_of_freedom = float_number_of_samples - 1
+# Power function
+power_of_two_float_sum_of_x_minus_mean = math.pow(float_sum_of_x_minus_mean, 2)
 #Compute the Variance
-Variance = float_sum_of_x_minus_mean / Degrees_of_freedom
+Variance = power_of_two_float_sum_of_x_minus_mean / Degrees_of_freedom
 import math
 #Compute Standard Deviation
 Standard_deviation = math.sqrt(Variance)
